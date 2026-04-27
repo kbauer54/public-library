a3f2542e5115409337879c0f18b34c49c2091b73
SaabiriinAbdi <120352653+SaabiriinAbdi@users.noreply.github.com>
2026-04-27 10:31:23 -0500

    changed back books.ts

--- frontend/src/api/books.ts
+++ frontend/src/api/books.ts
@@ -1,17 +1,7 @@
 import { api } from "./index";
 
 export const BooksAPI = {
-  getAll: async () => {
-    const res = await api.get("/api/books");
-
-    // Normalize all possible shapes:
-    // { data: { data: [...] } }
-    // { data: [...] }
-    // [...]
-    if (Array.isArray(res.data?.data)) return res.data.data;
-    if (Array.isArray(res.data)) return res.data;
-    return [];
-  },
+  getAll: () => api.get("/api/books"),
   getOne: (id: string) => api.get(`/api/books/${id}`),
   create: (data: any) => api.post("/api/books", data),
   update: (id: string, data: any) => api.put(`/api/books/${id}`, data),
